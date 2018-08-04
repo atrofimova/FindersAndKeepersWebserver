58e862c08108ef274e7bfc0f640d434924e944ba
Anna Trofimova <anna.trofimova91@gmail.com>
2018-08-05 03:40:03 +1000

    socketio communication updated

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
 var app=require('express')();
 var http =require('http').Server(app);
 var io=require('socket.io')(http);
+var fs = require("fs");
 
 //-------------------------------------------------------------------------//
 // Mongodb setup
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb://127.0.0.1/27017';
 
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
+var newItem;
 
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// Mongodb schema
-var Schema = mongoose.Schema;
-var ItemSchema = new Schema({
-	id: String,
-	picture: String,
-	q1: String,
-	q2: String,
-	q3: String,
-	a1: String,
-	a2: String,
-	a3: String,
-});
-
-
-var iid, ipicture, iq1, iq2, iq3, ia1, ia2, ia3;
-
-
+var contents = fs.readFileSync("dummy.json");
+var jsonContent = JSON.parse(contents);
 // Compile model from schema
-var ItemModel = mongoose.model('ItemModel', ItemSchema );
-
-
-
 
 
 //-------------------------------------------------------------------------//
@@ -46,40 +23,44 @@ io.on('connect', function(socket){
 
 	});
 	socket.on('picture', function(data){
-		ipicture =  data;
-		//socket.emit("putImage", data);
+		newItem.picture =  data;
+
 	});
 	socket.on('id', function(data){
-		iid = data;
 	});
 	socket.on('q1', function(data){
-		iq1 = data;
+		newItem.q1 = data;
 	});
 	socket.on('q2', function(data){
-		iq2 = data;
+		newItem.q2 = data;
 	});
 	socket.on('q3', function(data){
-		iq3 = data;
+		newItem.q3 = data;
 	});
 	socket.on('a1', function(data){
-		ia1 = data;
+		newItem.a1 = data;
 	});
 	socket.on('a2', function(data){
-		ia2 = data;
+		newItem.a2 = data;
 	});
 	socket.on('a3', function(data){
-		ia3 = data;
-	});
-	socket.on('create', function(data){
-		var item_instance = new ItemModel({ id: iid,  picture: ipicture, q1:iq1, q2:iq2, q3:iq3, a1:ia1, a2:ia2, a3:ia3});
-		item_instance.save(function (err) {
-			if (err) return handleError(err);
-			// saved!
-		});
+		newItem.a3 = data;
 	});
 
 	socket.on('disconnect', function(){
 		console.log("User disconnected " + socket.id);
+	});
+
+	socket.on('getNewItem', function(){
+		if (newItem == 0){
+			//
+		} else {
+			socket.emit("setImage", newItem.picture);
+		}
+	});
+
+	socket.on('collected', function(){
+		newItem = 0;
 	})
 });
 
